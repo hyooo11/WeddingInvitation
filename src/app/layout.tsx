7c2e938ea72981cd24e748ec468faa51f7097de6
hyooo11 <tlsgywls@naver.com>
2025-04-24 23:58:19 +0900

    feat: 모바일 확대 방지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,12 @@ import Script from "next/script";
 // const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "동현 ♥ 진희의 결혼식에 초대합니다.",
-  description: "노동현 & 박진희의 모바일 청첩장",
+  description: "노동현 ♥ 박진희의 모바일 청첩장",
   viewport: {
-     width: 'device-width',
-     initialScale: 1,
-     maximumScale: 1
-   }
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1
+  }
 };
 
 export default function RootLayout({
